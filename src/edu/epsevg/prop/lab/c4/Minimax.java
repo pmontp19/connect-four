10f2384b7035e52dd24c0edb48302f160d62d06d
Pere <pere.montpeo@decathlon.com>
2019-11-17 20:47:14 +0100

    update

--- src/edu/epsevg/prop/lab/c4/Minimax.java
+++ src/edu/epsevg/prop/lab/c4/Minimax.java
@@ -32,57 +32,91 @@ public class Minimax implements Jugador, IAuto {
         return nom;
     }
 
-    private ArrayList<Tauler> possiblesMoviments(Tauler t, int color) {
+    private ArrayList<Accio> possiblesMoviments(Tauler t, int color) {
         ArrayList<Accio> accions = new ArrayList<>();
         //ArrayList<Tauler> accions = new ArrayList<>();
         for (int c = 0; c < t.getMida(); c++) {
             if (t.movpossible(c)) {
                 Tauler child = new Tauler(t);
                 child.afegeix(c, color);
-                child.solucio(c, c)
-                Accio accio = new Accio(child, true)
-                accions.add(child);
+                if (child.solucio(c, color)) {
+                    Accio accio = new Accio(child, child.solucio(c, color), color);
+                    accions.add(accio);
+                } else {
+                    Accio accio = new Accio(child, false);
+                    accions.add(accio);
+                }
+
             }
         }
         return accions;
     }
-    
+
     private int getHeuristica(Tauler t) {
-        int liniesCurrent, liniesOther = 0;
-        
-        
+        int current, other = 0;
+
+        // comprovem horitzontals
+        for (int f = 0; f < t.getMida(); f++) {
+            for (int c = 0; c < t.getMida(); c++) {
+                int prevColor = t.getColor(f, c);
+                int currColor = t.getColor(f, c+1);
+                }
+            }
+        }
+
+        for (int i = 0; i < t.getMida(); i++) {
+            for (int j = 0; j < t.getMida(); j++) {
+
+            }
+        }
+
     }
 
-    private int minmax(Tauler t, int depth, boolean maximitzar) {
+    private int minmax(Accio a, int depth, boolean maximitzar) {
 
-        if (depth == 0 || !t.espotmoure() ){
-            return getHeuristica(Tauler t);
+        if (depth == 0 || a.isTerminal()) {
+            if (a.getGuanyador() == 1) {
+                return Integer.MAX_VALUE;
+            } else if (a.getGuanyador() == -1) {
+                return Integer.MIN_VALUE;
+            }
+            return getHeuristica(a.getT());
+
         }
         if (maximitzar) {
-            ArrayList<Tauler> childs = new ArrayList<>();
-            childs = possiblesMoviments(t, 1);
+            ArrayList<Accio> childs = new ArrayList<>();
+            childs = possiblesMoviments(a.getT(), 1);
             int valor = Integer.MIN_VALUE;
-            for (Tauler child : childs) {
+            for (Accio child : childs) {
                 valor = Math.max(valor, minmax(child, depth - 1, false));
             }
         } else {
-            ArrayList<Tauler> childs = new ArrayList<>();
-            childs = possiblesMoviments(t, -1);
+            ArrayList<Accio> childs = new ArrayList<>();
+            childs = possiblesMoviments(a.getT(), -1);
             int valor = Integer.MAX_VALUE;
-            for (Tauler child : childs) {
-                valor = Math.max(valor, minmax(child, depth - 1, false));
+            for (Accio child : childs) {
+                valor = Math.max(valor, minmax(child, depth - 1, true));
             }
         }
-
+        return -1;
     }
-    
+
     private class Accio {
+
         private Tauler t;
-        private boolean terminal;   
+        private boolean terminal;
+        private int guanyador;
 
         public Accio(Tauler t, boolean terminal) {
             this.t = t;
             this.terminal = terminal;
+            guanyador = -1;
+        }
+
+        public Accio(Tauler t, boolean terminal, int guanyador) {
+            this.t = t;
+            this.terminal = terminal;
+            this.guanyador = guanyador;
         }
 
         public Tauler getT() {
@@ -100,7 +134,14 @@ public class Minimax implements Jugador, IAuto {
         public void setTerminal(boolean terminal) {
             this.terminal = terminal;
         }
-        
-        
+
+        public int getGuanyador() {
+            return guanyador;
+        }
+
+        public void setGuanyador(int guanyador) {
+            this.guanyador = guanyador;
+        }
+
     }
 }
